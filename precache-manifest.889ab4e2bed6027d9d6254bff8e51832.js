ec931d7d8fa2d908d38f618988238dd9fbfe8424
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-09-01 09:15:59 +0300

    Updates

---
+++ precache-manifest.889ab4e2bed6027d9d6254bff8e51832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360aec076b6a088389202e004e3c2d39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae5a7c965f15da48ada6",
+    "url": "./static/css/main.eda69059.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5a7c965f15da48ada6",
+    "url": "./static/js/main.2c6d7a9f.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
